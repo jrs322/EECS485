7f8ec8b6628774daff6664637f9a0694fdfef1e5
Joshua Schlichting <42978321+jrs322@users.noreply.github.com>
2019-10-08 10:35:34 -0400

    Update ALU_logic.v

--- ALU_logic.v
+++ ALU_logic.v
@@ -1,35 +1,19 @@
-module logic_block(A, B, sel, C, error);
+module logic_block(A, B, sel, C);
   input[15:0] A, B;
   input[2:0] sel;
   output reg[15:0] C;
-  output reg error;
+  
   always @(A or B or sel)
     begin
       case(sel)
-        3'b000: //and
+        3'b000: C = A & B;
+        3'b001: C = A | B;
+        3'b010: C = A ^ B; 
+        3'b100: C = ~A;
+        default: 
           begin
-            error <= 0; 
-            C <= A & B;
-          end
-        3'b001: //or
-          begin
-            error <= 0;
-            C <= A | B;
-          end
-        3'b010: //xor
-          begin
-            error <= 0;
-            C <= A ^ B;
-          end
-        3'b011: //not
-          begin
-            error <= 0;
-            C <= ~A;
-          end
-        default:
-          begin
-            error = 1;
-            $display("Error, not correct sel signal");
+          	C = 16'b0;
+            $display("Default behavior error")
           end
       endcase
     end
